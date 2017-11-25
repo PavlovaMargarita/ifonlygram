90e2aa884c6d3e59851da00474441be20fbb28f9
ViktoryiaYatskova <yatskovaVD@gmail.com>
2017-11-25 20:41:55 +0300

    [ui] complete relocation to profile page

--- client/js/main-page.js
+++ client/js/main-page.js
@@ -2,16 +2,6 @@ document.addEventListener('DOMContentLoaded', onLoadPage);
 
 function onLoadPage() {
     fetchCategories().then(updateCategories);
-    initListeners();
-}
-
-function initListeners() {
-    document.getElementById('run').addEventListener('click', () => {
-        const name = document.getElementById('username').value;
-        const category = document.getElementById('categories').value;
-        const indexLocation = window.location.href.match(/^(.*)\/[a-z]*\.html\?+.*$/)[1];
-        window.location.replace(`${indexLocation}/html/profile.html?name=${name}&category=${category}`);
-    });
 }
 
 function fetchCategories() {
--- client/js/profile-page.js
+++ client/js/profile-page.js
@@ -3,21 +3,26 @@ document.addEventListener('DOMContentLoaded', onProfileLoadPage);
 function onProfileLoadPage() {
     if (location.href.indexOf('?') < 0) return;
 
-    const { name, category} = parseProfileQueryParams();
-    sendGenerateProfile(name, category).then((data) => {
-        debugger;
-    });
+    const { name, category } = parseProfileQueryParams();
+    if (name && category) {
+        sendGenerateProfile(name, category).then((data) => {
+            debugger;
+        });
+    }
 }
 
 function parseProfileQueryParams() {
-    const queryParamsString = location.href.match(/^.*(\?.*)$/)[1];
-    const queryParams = queryParamsString.split('&');
+    const queryParamsString = location.href.match(/^.*\?(.*)$/)[1];
+    const queryParams = decodeURIComponent(queryParamsString).split('&');
     let name;
     let category;
     queryParams.forEach((param) => {
-        name = param.match(/^name\=(\w*)$/)[1];
-        if(!name) {
-            category = param.match(/^blogCategory=\=(\w*)$/)[1];
+        const parsedName = param.match(/^username\=(.*)$/);
+        if(parsedName && !name) {
+            name = parsedName[1];
+        } else if (!category) {
+            const parsedCategory = param.match(/^category\=(.*)$/);
+            category = parsedCategory && parsedCategory[1];
         }
     });
     return { name, category };
