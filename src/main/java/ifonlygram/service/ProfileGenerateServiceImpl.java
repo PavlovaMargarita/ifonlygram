f74a0121b4263a16f2787e88fa07f921cf1171df
Ryta Pavlova <ryta.pavlova@godel.first-utility.com>
2017-11-25 14:28:24 +0300

    set description, location and tags to publication

--- src/main/java/ifonlygram/service/ProfileGenerateServiceImpl.java
+++ src/main/java/ifonlygram/service/ProfileGenerateServiceImpl.java
@@ -8,10 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 @Component
 public class ProfileGenerateServiceImpl implements ProfileGenerateService {
+    private static final int TAG_COUNT = 15;
 
     @Autowired
     private PublicationGenerateService publicationGenerateService;
@@ -27,15 +30,34 @@ public class ProfileGenerateServiceImpl implements ProfileGenerateService {
 
         InfoWiki infoWiki = infoWikiRetrieveService.getInfoWikiByName(name);
 
+        List<String> allTagsForBlogCategory = tagRetrieveService.getAllTags(blogCategory);
+
         List<Publication> allPublication = new ArrayList<Publication>();
 
         List<String> wikiParameters = getParametersFromInfoWiki(infoWiki);
-        for(String wikiParameter : wikiParameters) {
+        for (String wikiParameter : wikiParameters) {
             String tag = getRandomTag(blogCategory);
-            List<Publication> publications = publicationGenerateService.generatePublications(tag, name, wikiParameter);
+            List<Publication> publications = publicationGenerateService.generatePublications(tag, name, wikiParameter); //contain image url and one tag
+
+            // set description, location and add tags
+            publications.forEach(post -> {
+                //set description
+                post.setDescription(tagRetrieveService.getRandomDescriptionByTagAndBlogCategory(tag, blogCategory));
+
+                //set location
+                if (infoWiki.getPlaces().contains(wikiParameter)) {
+                    post.setLocation(wikiParameter);
+                }
+
+                //add tags
+                post.getTags().addAll(getRandomTags(allTagsForBlogCategory, TAG_COUNT));
+
+            });
+
             allPublication.addAll(publications);
         }
 
+        Collections.shuffle(allPublication);
         Profile profile = new Profile();
         profile.setName(name);
         profile.setPublications(allPublication);
@@ -55,5 +77,12 @@ public class ProfileGenerateServiceImpl implements ProfileGenerateService {
 
     private String getRandomTag(BlogCategory blogCategory) {
         return tagRetrieveService.getRandomTag(blogCategory);
+    }
+
+    private List<String> getRandomTags(List<String> allTagsForBlogCategory, int count) {
+        List<String> copy = new LinkedList<String>(allTagsForBlogCategory);
+        Collections.shuffle(copy);
+        int currentTagCount = allTagsForBlogCategory.size();
+        return copy.subList(0, currentTagCount < count ? currentTagCount : count);
     }
 }
--- src/main/java/ifonlygram/service/PublicationGenerateServiceImpl.java
+++ src/main/java/ifonlygram/service/PublicationGenerateServiceImpl.java
@@ -6,11 +6,13 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import static ifonlygram.util.SearchImage.findImageUrlByParameters;
 
 @Component
 public class PublicationGenerateServiceImpl implements PublicationGenerateService {
+    private static final Random random = new Random();
     @Override
     public List<Publication> generatePublications(final String tag, final String name, final String infoWiki) {
 
@@ -35,6 +37,6 @@ public class PublicationGenerateServiceImpl implements PublicationGenerateServic
     }
 
     private boolean shouldWeUseNameInSearch() {
-        return true;
+        return random.nextBoolean();
     }
 }
