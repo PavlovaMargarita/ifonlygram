50e5ba6047092db2fa8aac6aad5ca6c371344a49
ViktoryiaYatskova <yatskovaVD@gmail.com>
2017-11-26 00:29:10 +0300

    [UI] add publications

--- client/js/profile-page.js
+++ client/js/profile-page.js
@@ -5,9 +5,8 @@ function onProfileLoadPage() {
 
     const { name, category } = parseProfileQueryParams();
     if (name && category) {
-        sendGenerateProfile(name, category).then((data) => {
-            debugger;
-        });
+        sendGenerateProfile(name, category)
+            .then(parseProfileData);
     }
 }
 
@@ -30,4 +29,51 @@ function parseProfileQueryParams() {
 
 function sendGenerateProfile(name, category) {
     return sendRequest(`/generateProfile?name=${name}&blogCategory=${category}`, 'GET');
+}
+
+function parseProfileData(data) {
+    const { name, profileDescription, profilePicture, publications } = data;
+    setImage('profile-image', profilePicture);
+    setText('profile-name', name);
+    setText('description', profileDescription);
+    setText('publications-number', publications.length);
+
+    const publicationsElement = document.getElementById('publications');
+    const rowsNumber = Math.ceil(publications.length / 3);
+
+    for(let i = 0; i < rowsNumber; i++) {
+        publicationsElement.appendChild(
+            createPublicationsRow(name, publications[i], publications[i+1], publications[i+2])
+        );
+    }
+}
+
+function createPublicationsRow(ownerName, ...publications) {
+    const row = document.createElement('div');
+    row.classList.add('publications-row');
+    publications.forEach(publication => row.appendChild(createPublication(publication, ownerName)));
+    return row;
+}
+function createPublication(publication = {}, ownerName) {
+    const { description = '', imageUrl = '', location = '', tags = []} = publication;
+    const publicationElement = document.createElement('div');
+    publicationElement.classList.add('_mck9w', '_gvoze', 'row-element');
+    publicationElement.innerHTML =
+        `<a href="publication.html?owner=${ownerName}&location=${location}">` +
+            `<div class="_e3il2">` +
+                `<div class="_4rbun">` +
+                    `<img
+                        class="_2di5p publ-image"
+                        alt="${tags.join(' #')}"
+                        src="${imageUrl}">` +
+                `</div>` +
+                `<div class="_si7dy"></div>` +
+            `</div>` +
+            `<div class="_lxd52">` +
+                `<div class="_jnyq2">` +
+                    `<span class="_c3aqk _8scx2 coreSpriteSidecarIconLarge">Post</span>` +
+                `</div>` +
+            `</div>` +
+        `</a>`;
+    return publicationElement;
 }
--- client/server.js
+++ client/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 
-const HTML_LOCATION = '/static/html/';
+const HTML_LOCATION = '';
+const CSS_LOCATION = '';
 
 app.get('/*', function(req, res){
     console.log(__dirname + req.path);
@@ -12,8 +13,20 @@ app.get('/*', function(req, res){
     if (req.path.indexOf('.html') > 0) {
         return res.sendFile(__dirname + HTML_LOCATION + req.path);
     }
+    if (req.path.indexOf('.css') > 0) {
+        return res.sendFile(__dirname + CSS_LOCATION + req.path);
+    }
 
     return res.sendFile(__dirname + req.path);
+});
+
+// print process.argv
+process.argv.forEach(function (val, index, array) {
+    const parsedServer = val.match(/^server\=([\w\d\.]*)/);
+    if (parsedServer) {
+        server = parsedServer[1];
+        console.log(server);
+    }
 });
 
 app.listen(3000);
