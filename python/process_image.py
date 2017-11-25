4ed05383e9075ec2240fb19d309dab539c4ed2b7
NastassiaM <nastassia.mitskevich@gmail.com>
2017-11-25 22:28:50 +0300

    process_image.py refactored; error_handling added

--- python/process_image.py
+++ python/process_image.py
@@ -1,10 +1,12 @@
-import numpy as np
-import cv2
 import argparse
+import os
+
+import cv2
+import numpy as np
 from PIL import Image
 from PIL import ImageOps
 
-THRESHOLD = 0.2
+THRESHOLD = 0.6
 SIZE = 640
 
 # initialize the list of class labels MobileNet SSD was trained to
@@ -15,36 +17,6 @@ CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
            "sofa", "train", "tvmonitor"]
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
-class Box(object):
-    def __init__(self, rect, prob, label):
-        self._rect = rect
-        self._prob = prob
-        self._label = label
-
-    @property
-    def rect(self):
-        return self._rect
-
-    @rect.setter
-    def rect(self, value):
-        self._rect = value
-
-    @property
-    def prob(self):
-        return self._prob
-
-    @prob.setter
-    def prob(self, value):
-        self._prob = value
-
-    @property
-    def label(self):
-        return self._label
-
-    @label.setter
-    def label(self, value):
-        self._label = value
-
 
 class Rect():
     def __init__(self, start_x=0, start_y=0, end_x=0, end_y=0):
@@ -54,61 +26,89 @@ class Rect():
         self.end_y = end_y
 
 
-def union(a, b):
-    start_x = min(a[0], b[0])
-    start_y = min(a[1], b[1])
-    end_x = max(a[2], b[2])
-    end_y = max(a[3], b[3])
-    return start_x, start_y, end_x, end_y
+def get_img_detections_and_dims(file):
+    print("get_img_detections_and_dims() started...")
 
+    dir_name = os.path.dirname(__file__)
+    prototxt_file = os.path.join(dir_name, "MobileNetSSD_deploy.prototxt")
+    caffemodel_file = os.path.join(dir_name, "MobileNetSSD_deploy.caffemodel")
 
-def process_image(file = "Fashion.jpg"):
-    print("Process image start")
-    net = cv2.dnn.readNetFromCaffe("python/MobileNetSSD_deploy.prototxt", "python/MobileNetSSD_deploy.caffemodel")
+    net = cv2.dnn.readNetFromCaffe(prototxt_file, caffemodel_file)
     image = cv2.imread(file)
     (h, w) = image.shape[:2]
     blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
     # pass the blob through the network and obtain the detections and predictions
     net.setInput(blob)
-    detections = net.forward()
+    return net.forward(), (h, w)
+
+
+def get_detections_bounding_boxes(detections, img_height, img_width):
+    print("get_detections_bounding_boxes() started...")
 
     boxes = []
     for i in np.arange(0, detections.shape[2]):
-        # extract the confidence (i.e., probability) associated with the prediction
         confidence = detections[0, 0, i, 2]
-
         # filter out weak detections by ensuring the `confidence` is greater than the minimum confidence
         if confidence > THRESHOLD:
-            # extract the index of the class label from the `detections`,
-            # then compute the (x, y)-coordinates of the bounding box for the object
-            idx = int(detections[0, 0, i, 1])
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
+            # idx = int(detections[0, 0, i, 1])
+            box = detections[0, 0, i, 3:7] * np.array([img_width, img_height, img_width, img_height])
+            (start_x, start_y, end_x, end_y) = box.astype("int")
+            boxes.append(Rect(start_x, start_y, end_x, end_y))
+    return boxes
 
-            boxes.append(Box((startX, startY, endX, endY), confidence, CLASSES[idx]))
 
-    print("some objects found")
-    union_rect = None
-    for i in range(len(boxes)):
+def union(a, b):
+    start_x = min(a.start_x, b.start_x)
+    start_y = min(a.start_y, b.start_y)
+    end_x = max(a.end_x, b.end_x)
+    end_y = max(a.end_y, b.end_y)
+    return Rect(start_x, start_y, end_x, end_y)
+
+
+def get_union_bounding_box_center(bounding_boxes, img_height, img_width):
+    print("get_union_bounding_box_center() started...")
+
+    union_rect = Rect()
+    for i in range(len(bounding_boxes)):
         if i is 0:
-            union_rect = boxes[i].rect
+            union_rect = bounding_boxes[i]
         else:
-            union_rect = union(union_rect, boxes[i].rect)
+            union_rect = union(union_rect, bounding_boxes[i])
 
-    # expand union_rect
-    union_rect = (max(0, union_rect[0] - 10), max(0, union_rect[1] - 10), min(w, union_rect[2] + 10),
-                  min(h, union_rect[3] + 10))
+    return (union_rect.start_x + union_rect.end_x) / 2 / img_width, \
+           (union_rect.start_y + union_rect.end_y) / 2 / img_height
 
-    rect_center = (union_rect[0] + union_rect[2]) / 2 / w, (union_rect[1] + union_rect[3]) / 2 / h
+
+def crop_and_save_img(file, crop_center):
+    print("crop_and_save_img() started...")
 
     im = Image.open(file)
-    fit = ImageOps.fit(im, (SIZE, SIZE), centering=rect_center)
-    print("preparing to send cropped image")
-    fit.save("croppedimages/processed.jpg")
+    fit = ImageOps.fit(im, (SIZE, SIZE), centering=crop_center)
+    dir_name = os.path.dirname(__file__)
+    file_name = os.path.join(dir_name, '../croppedimages/', 'processed.jpg')
+    fit.save(file_name)
+
+
+def process_image(file):
+    print("process_image() started...")
+
+    dir_name = os.path.dirname(__file__)
+    file = os.path.join(dir_name, '../croppedimages/', file)
+
+    detections, (img_height, img_width) = get_img_detections_and_dims(file)
+
+    bounding_boxes = get_detections_bounding_boxes(detections, img_height, img_width)
+
+    crop_center = (0.5, 0.5)
+    if bounding_boxes:  # some objects found
+        crop_center = get_union_bounding_box_center(bounding_boxes, img_height, img_width)
+
+    crop_and_save_img(file, crop_center)
+
 
 if __name__ == '__main__':
     # construct the argument parse and parse the arguments
     ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--image", required=True, help="path to input image")
+    ap.add_argument("-i", "--image", required=True, help="file name (in /croppedimages folder")
     args = vars(ap.parse_args())
     process_image(args["image"])
