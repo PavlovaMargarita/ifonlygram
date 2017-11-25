c49e20b9ff62d19f3dda38d56e8209543296cd70
Ryta Pavlova <ryta.pavlova@godel.first-utility.com>
2017-11-25 16:21:02 +0300

    add CrossOrigin and logging

--- src/main/java/ifonlygram/rest/IfOnlyGramRest.java
+++ src/main/java/ifonlygram/rest/IfOnlyGramRest.java
@@ -4,6 +4,7 @@ import ifonlygram.dto.BlogCategory;
 import ifonlygram.dto.Profile;
 import ifonlygram.service.ProfileGenerateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
+@CrossOrigin
 public class IfOnlyGramRest {
 
     @Autowired
@@ -18,11 +20,13 @@ public class IfOnlyGramRest {
 
     @RequestMapping(value = "/generateProfile", method = GET)
     public Profile getProfile(@RequestParam("name") final String name, @RequestParam("blogCategory") final BlogCategory blogCategory) {
+        System.out.println(String.format("call generateProfile with name %s and blog category %s", name, blogCategory));
         return profileGenerateService.generateProfile(name, blogCategory);
     }
 
     @RequestMapping(value = "/blogCategories", method = GET)
     public BlogCategory[] getBlogCategories() {
+        System.out.println("call blogCategories");
         return BlogCategory.values();
     }
 
